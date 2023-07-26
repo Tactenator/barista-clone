025b13721a5984894867801c0e4f9bcf8a2a3b8d
Tactenator <trevor.mclaurine@gmail.com>
2023-07-26 12:20:32 -0500

    updating landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        "hero-background": "#eae7de"
+        "hero-background": "#eae7de",
+        "footer": "#1a1a1a"
       },
       colors: {
         "gold-text": "#c7a17a"
